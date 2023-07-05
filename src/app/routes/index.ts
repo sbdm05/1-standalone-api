8f2f989a89d31a5c38ea23b3bc704f4e00a7f42b
Sonia-Baibou <s.baibou@ohmycode.io>
2023-07-05 13:25:54 +0200

    Jour 3 - matin

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from '../app.component';
+import { AccueilComponent } from '../components/accueil/accueil.component';
 import { ListeComponent } from '../components/liste/liste.component';
 
 export const mainRoutes : Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: AccueilComponent },
   { path: 'liste', component: ListeComponent },
 ];
